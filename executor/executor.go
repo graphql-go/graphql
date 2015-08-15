e3d6580007d3bd7d56573a065b480bbbad426660
Chris Ramón <rchristian.ramon@gmail.com>
2015-08-15 00:43:24 -0500

    executor wip

--- executor/executor.go
+++ executor/executor.go
@@ -1,73 +1,61 @@
 package executor
 
 import (
-	"reflect"
+	"fmt"
+	"log"
 
 	"github.com/chris-ramon/graphql-go/errors"
 	"github.com/chris-ramon/graphql-go/language/ast"
-	"github.com/chris-ramon/graphql-go/language/fd"
 	"github.com/chris-ramon/graphql-go/language/kinds"
-	"github.com/chris-ramon/graphql-go/language/od"
 	"github.com/chris-ramon/graphql-go/types"
 )
 
-type ExecutionResult struct {
-	Data   interface{}
-	Errors []errors.GraphQLFormattedError
-}
-
 type ExecuteParams struct {
 	Schema        types.GraphQLSchema
-	Result        types.GraphQLResult
 	Root          map[string]interface{}
 	AST           ast.Document
 	OperationName string
 	Args          map[string]string
 }
 
-func Execute(p ExecuteParams, r chan types.GraphQLResult) {
+func Execute(p ExecuteParams, resultChan chan *types.GraphQLResult) {
 	var errors []error
+	var result types.GraphQLResult
 	params := BuildExecutionCtxParams{
 		Schema:        p.Schema,
-		Result:        p.Result,
 		Root:          p.Root,
 		AST:           p.AST,
 		OperationName: p.OperationName,
 		Args:          p.Args,
 		Errors:        errors,
+		Result:        &result,
+		ResultChan:    resultChan,
 	}
 	exeContext := buildExecutionContext(params)
+	if result.HasErrors() {
+		return
+	}
 	eOperationParams := ExecuteOperationParams{
 		ExecutionContext: exeContext,
 		Root:             p.Root,
 		Operation:        exeContext.Operation,
 	}
-	executeOperation(eOperationParams, r)
-}
-
-type ExecutionContext struct {
-	Schema    types.GraphQLSchema
-	Result    types.GraphQLResult
-	Fragments map[string]fd.FragmentDefinition
-	Root      map[string]interface{}
-	Operation od.OperationDefinition
-	Variables map[string]interface{}
-	Errors    []error
+	executeOperation(eOperationParams, resultChan)
 }
 
 type ExecuteOperationParams struct {
 	ExecutionContext ExecutionContext
 	Root             map[string]interface{}
-	Operation        od.OperationDefinition
+	Operation        ast.Definition
 }
 
-func executeOperation(p ExecuteOperationParams, r chan types.GraphQLResult) {
+func executeOperation(p ExecuteOperationParams, r chan *types.GraphQLResult) {
 	//TODO: mutation operation
-	operationType := getOperationRootType(p.ExecutionContext.Schema, p.Operation)
+	operationType := getOperationRootType(p.ExecutionContext.Schema, p.Operation, r)
 	collectFieldsParams := CollectFieldsParams{
 		ExeContext:    p.ExecutionContext,
 		OperationType: operationType,
-		SelectionSet:  p.Operation.SelectionSet,
+		SelectionSet:  p.Operation.GetSelectionSet(),
 	}
 	fields := collectFields(collectFieldsParams)
 	executeFieldsParams := ExecuteFieldsParams{
@@ -79,8 +67,29 @@ func executeOperation(p ExecuteOperationParams, r chan types.GraphQLResult) {
 	executeFields(executeFieldsParams, r)
 }
 
-func getOperationRootType(schema types.GraphQLSchema, operation od.OperationDefinition) (r types.GraphQLObjectType) {
-	return r
+func getOperationRootType(schema types.GraphQLSchema, operation ast.Definition, r chan *types.GraphQLResult) (objType types.GraphQLObjectType) {
+	switch operation.GetOperation() {
+	case "query":
+		return schema.GetQueryType()
+	case "mutation":
+		mutationType := schema.GetMutationType()
+		if mutationType.Name != "" {
+			var result types.GraphQLResult
+			result.Errors = append(result.Errors, errors.GraphQLFormattedError{
+				Message: "Schema is not configured for mutations",
+			})
+			r <- &result
+			return objType
+		}
+		return mutationType
+	default:
+		var result types.GraphQLResult
+		result.Errors = append(result.Errors, errors.GraphQLFormattedError{
+			Message: "Can only execute queries and mutations",
+		})
+		r <- &result
+		return objType
+	}
 }
 
 type CollectFieldsParams struct {
@@ -103,31 +112,40 @@ type ExecuteFieldsParams struct {
 	Fields           map[string][]ast.Field
 }
 
-func executeFields(p ExecuteFieldsParams, r chan types.GraphQLResult) {
+func executeFields(p ExecuteFieldsParams, resultChan chan *types.GraphQLResult) {
 	var result types.GraphQLResult
-	mutable := reflect.ValueOf(p.ExecutionContext.Result.Data).Elem()
-	mutable.FieldByName("Name").SetString("R2-D2")
-	r <- result
+	//mutable := reflect.ValueOf(p.ExecutionContext.Result.Data).Elem()
+	//mutable.FieldByName("Name").SetString("R2-D2")
+	resultChan <- &result
 }
 
 type BuildExecutionCtxParams struct {
 	Schema        types.GraphQLSchema
-	Result        types.GraphQLResult
 	Root          map[string]interface{}
 	AST           ast.Document
 	OperationName string
 	Args          map[string]string
 	Errors        []error
+	Result        *types.GraphQLResult
+	ResultChan    chan *types.GraphQLResult
+}
+
+type ExecutionContext struct {
+	Schema    types.GraphQLSchema
+	Fragments map[string]ast.Definition
+	Root      map[string]interface{}
+	Operation ast.Definition
+	Variables map[string]interface{}
+	Errors    []error
 }
 
 func buildExecutionContext(p BuildExecutionCtxParams) (eCtx ExecutionContext) {
-	eCtx.Schema = p.Schema
-	eCtx.Result = p.Result
-	var operations map[string]ast.Definition
-	var fragments map[string]ast.Definition
+	operations := make(map[string]ast.Definition)
+	fragments := make(map[string]ast.Definition)
 	for _, statement := range p.AST.Definitions {
 		switch statement.GetKind() {
 		case kinds.OperationDefinition:
+			log.Println("kinds.OperationDefinition")
 			key := ""
 			if statement.GetName().Value != "" {
 				key = statement.GetName().Value
@@ -135,34 +153,42 @@ func buildExecutionContext(p BuildExecutionCtxParams) (eCtx ExecutionContext) {
 			operations[key] = statement
 			break
 		case kinds.FragmentDefinition:
+			log.Println("kinds.FragmentDefinition")
 			fragments[statement.GetName().Value] = statement
 			break
+		default:
+			log.Println("default")
 		}
 	}
+	log.Printf("debug - operations: %v", operations)
 	if (p.OperationName == "") && (len(operations) != 1) {
 		p.Result.Errors = append(p.Result.Errors, errors.GraphQLFormattedError{
 			Message: "Must provide operation name if query contains multiple operations",
 		})
+		p.ResultChan <- p.Result
 		return eCtx
 	}
 	var opName string
 	if p.OperationName == "" {
-		var i int
 		for k, _ := range operations {
 			opName = k
-			if i == 0 {
-				break
-			}
+			break
 		}
-		p.OperationName = opName
 	}
-	//TODO: validate if operations[opName] is an empty struct
-	//var operation ast.Definition
-	//if (operations[opName].(od.OperationDefinition) == od.OperationDefinition{}) || (operations[opName].(fd.FragmentDefinition) == fd.FragmentDefinition{}) {
-	//p.Result.Errors = append(p.Result.Errors, errors.GraphQLFormattedError{
-	//Message: fmt.Sprintf("Unknown operation name: %s", opName),
-	//})
-	//return eCtx
-	//}
+	operation, found := operations[opName]
+	if !found {
+		var result types.GraphQLResult
+		result.Errors = append(result.Errors, errors.GraphQLFormattedError{
+			Message: fmt.Sprintf("Unknown operation name: %s", opName),
+		})
+		return eCtx
+	}
+	variables := GetVariableValues(p.Schema, operation.GetVariableDefinitions(), p.Args)
+	eCtx.Schema = p.Schema
+	eCtx.Fragments = fragments
+	eCtx.Root = p.Root
+	eCtx.Operation = operation
+	eCtx.Variables = variables
+	eCtx.Errors = p.Errors
 	return eCtx
 }
