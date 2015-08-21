319f459645563bea3629be804ae28322c167fc4c
Chris Ramón <rchristian.ramon@gmail.com>
2015-08-20 22:43:21 -0500

    add more language kinds constants

--- language/kinds/kinds.go
+++ language/kinds/kinds.go
@@ -1,5 +1,19 @@
 package kinds
 
-const OperationDefinition = "OperationDefinition"
-
-const FragmentDefinition = "FragmentDefinition"
+const (
+	OperationDefinition = "OperationDefinition"
+	FragmentDefinition  = "FragmentDefinition"
+	Document            = "Document"
+	SelectionSet        = "SelectionSet"
+	Name                = "Name"
+	Directive           = "Directive"
+	VariableDefinition  = "VariableDefinition"
+	Variable            = "Variable"
+	ListType            = "ListType"
+	NonNullType         = "NonNullType"
+	InlineFragment      = "InlineFragment"
+	FragmentSpread      = "FragmentSpread"
+	Field               = "Field"
+	Array               = "Array"
+	Argument            = "Argument"
+)
