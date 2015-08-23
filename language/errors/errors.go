afb52ef89b4f36d9a9db57b584fa85b71339b1c2
Chris Ramón <rchristian.ramon@gmail.com>
2015-08-23 02:28:28 -0500

    add language syntax error func

--- language/errors/errors.go
+++ language/errors/errors.go
@@ -3,26 +3,44 @@ package languageerrors
 import (
 	"errors"
 	"fmt"
+	"regexp"
 
 	"github.com/chris-ramon/graphql-go/language/location"
 	"github.com/chris-ramon/graphql-go/language/source"
 )
 
 func Error(s *source.Source, position int, description string) error {
-	location := location.GetLocation(s, position)
-	err := fmt.Sprintf(`
-	Syntax error %s (%s:%s) %s
-	`, s.Name, location.Line, location.Column, description)
-	//err := fmt.Sprintf(`
-	//Syntax error %s (%s:%s) %s \n\n %s
-	//`, s.Name, location.Line, location.Column, description, highlightSourceAtLocation(s, location))
+	l := location.GetLocation(s, position)
+	err := fmt.Sprintf("Syntax Error %s (%d:%d) %s.\n\n%s", s.Name, l.Line, l.Column, description, highlightSourceAtLocation(s, l))
 	return errors.New(err)
 }
 
-//func highlightSourceAtLocation(s source.Source, location.SourceLocation) {
-//line := location.Line
-//prevLineNum := fmt.Sprintf("%d", (line - 1))
-//lineNum := fmt.Sprintf("%d", line)
-//nextLineNum := fmt.Sprintf("%d", (line + 1))
-//padLen := len(nextLineNum)
-//}
+func highlightSourceAtLocation(s *source.Source, l location.SourceLocation) string {
+	line := l.Line
+	prevLineNum := fmt.Sprintf("%d", (line - 1))
+	lineNum := fmt.Sprintf("%d", line)
+	nextLineNum := fmt.Sprintf("%d", (line + 1))
+	padLen := len(nextLineNum)
+	lines := regexp.MustCompile("\r\n|[\n\r\u2028\u2029]").Split(s.Body, -1)
+	var highlight string
+	if line >= 2 {
+		highlight += fmt.Sprintf("%s: %s\n", lpad(padLen, prevLineNum), lines[line-2])
+	}
+	highlight += fmt.Sprintf("%s: %s\n", lpad(padLen, lineNum), lines[line-1])
+	for i := 1; i < (2 + padLen + l.Column); i++ {
+		highlight += " "
+	}
+	highlight += "^\n"
+	if line < len(lines) {
+		highlight += fmt.Sprintf("%s: %s\n", lpad(padLen, nextLineNum), lines[line])
+	}
+	return highlight
+}
+
+func lpad(l int, s string) string {
+	var r string
+	for i := 1; i < (l - len(s) + 1); i++ {
+		r += " "
+	}
+	return r + s
+}
