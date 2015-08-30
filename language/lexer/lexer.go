08c72e2b112f147bd8d0f3665c7060176249dcee
Chris Ramón <rchristian.ramon@gmail.com>
2015-08-30 17:20:10 -0500

    unify readName case

--- language/lexer/lexer.go
+++ language/lexer/lexer.go
@@ -362,10 +362,8 @@ func readToken(s *source.Source, fromPosition int) (Token, error) {
 		82, 83, 84, 85, 86, 87, 88, 89, 90:
 		return readName(s, position), nil
 	// _
-	case 95:
-		return readName(s, position), nil
 	// a-z
-	case 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+	case 95, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
 		111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122:
 		return readName(s, position), nil
 	// -
