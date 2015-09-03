a13e769402ebf8b5dd9bb19beeb80885afae5ef1
Chris Ramón <rchristian.ramon@gmail.com>
2015-09-02 20:58:50 -0500

    add first parser test

--- language/parser/parser_test.go
+++ language/parser/parser_test.go
@@ -1,16 +1,64 @@
 package parser
 
-import "testing"
+import (
+	"reflect"
+	"testing"
 
-//func TestErrors(t *testing.T) {
-//s := `{ ...MissingOn }
-//fragment MissingOn Type`
-//_, err := Parse(ParseParams{Source: s})
-//expected := "Syntax Error GraphQL (2:20) Expected \"on\", found Name \"Type\""
-//if err == nil {
-//t.Fatalf("expected an error, got nil")
-//}
-//if err.Error() != expected {
-//t.Errorf("wrong result, expected: %v, got: %v", expected, err.Error())
-//}
-//}
+	"github.com/chris-ramon/graphql-go/language/ast"
+	"github.com/chris-ramon/graphql-go/language/od"
+)
+
+func TestAcceptsOptionToNotIncludeSource(t *testing.T) {
+	opts := ParseOptions{
+		NoSource: true,
+	}
+	params := ParseParams{
+		Source:  "{ field }",
+		Options: opts,
+	}
+	document, err := Parse(params)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	oDef := od.OperationDefinition{
+		Kind: "OperationDefinition",
+		Loc: ast.Location{
+			Start: 0, End: 9,
+		},
+		Operation:  "query",
+		Directives: []ast.Directive{},
+		SelectionSet: ast.SelectionSet{
+			Kind: "SelectionSet",
+			Loc: ast.Location{
+				Start: 0, End: 9,
+			},
+			Selections: []interface{}{
+				ast.Field{
+					Kind: "Field",
+					Loc: ast.Location{
+						Start: 2, End: 7,
+					},
+					Name: ast.Name{
+						Kind: "Name",
+						Loc: ast.Location{
+							Start: 2, End: 7,
+						},
+						Value: "field",
+					},
+					Arguments:  []ast.Argument{},
+					Directives: []ast.Directive{},
+				},
+			},
+		},
+	}
+	expectedDocument := ast.Document{
+		Kind: "Document",
+		Loc: ast.Location{
+			Start: 0, End: 9,
+		},
+		Definitions: []ast.Definition{&oDef},
+	}
+	if !reflect.DeepEqual(document, expectedDocument) {
+		t.Fatalf("unexpected document, expected: %v, got: %v", expectedDocument, document)
+	}
+}
