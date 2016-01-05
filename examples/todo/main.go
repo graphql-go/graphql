2be3d3c256100302f19e18ff6f8d25fe076c1ddb
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-01-05 16:29:51 +0100

    Handle  request, serve content from the `static` dir
    Add cli message that we are serving web app at 'http://localhost:8080/'

--- examples/todo/main.go
+++ examples/todo/main.go
@@ -171,15 +171,21 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 }
 
 func main() {
-
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		result := executeQuery(r.URL.Query()["query"][0], schema)
 		json.NewEncoder(w).Encode(result)
 	})
 
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	// Display some basic instructions
 	fmt.Println("Now server is running on port 8080")
 	fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
 	fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
 	fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
+	fmt.Println("Access the web app via browser at 'http://localhost:8080'")
+
 	http.ListenAndServe(":8080", nil)
 }
