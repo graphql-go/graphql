bac5e65bf28e4645d30ef6620a1d11d98b214e79
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-01-06 21:19:21 +0100

    Add "Add task" functionality, use the mutation

--- examples/todo/static/assets/js/app.js
+++ examples/todo/static/assets/js/app.js
@@ -1,25 +1,50 @@
+var handleTodoList = function(object) {
+  var todos = object;
+
+  if (!todos.length) {
+    $('.todo-list-container').append('<p>There are no tasks for you today</p>');
+  }
+
+  $.each(todos, function(i, v) {
+    var doneHtml = '<input id="' + v.id + '" type="checkbox"' + (v.done ? ' checked="checked"' : '') + '>';      
+    var labelHtml = '<label for="' + v.id + '">' + doneHtml + v.text + '</label>';
+    var itemHtml = '<div class="todo-item">' + labelHtml + '</div>';
+    
+    $('.todo-list-container').append(itemHtml);
+  });
+};
+
 var loadTodos = function() {
   $.ajax({
     url: "/graphql?query={todoList{id,text,done}}"
   }).done(function(data) {
     console.log(data);
     var dataParsed = JSON.parse(data);
-    var todos = dataParsed.data.todoList;
+    handleTodoList(dataParsed.data.todoList);
+  });
+};
 
-    if (!todos.length) {
-      $('.todo-list-container').append('<p>There are no tasks for you today</p>');
-    }
+var addTodo = function(todoText) {
+  if (!todoText || todoText === "") {
+    alert('Please specify a task');
+    return;
+  }
 
-    $.each(todos, function(i, v) {
-      var doneHtml = '<input id="' + v.id + '" type="checkbox"' + (v.done ? ' checked="checked"' : '') + '>';      
-      var labelHtml = '<label for="' + v.id + '">' + doneHtml + v.text + '</label>';
-      var itemHtml = '<div class="todo-item">' + labelHtml + '</div>';
-      
-      $('.todo-list-container').append(itemHtml);
-    });
+  $.ajax({
+    url: '/graphql?query=mutation+_{createTodo(text:"' + todoText + '"){id,text,done}}'
+  }).done(function(data) {
+    console.log(data);
+    var dataParsed = JSON.parse(data);
+    var todoList = [dataParsed.data.createTodo];
+    handleTodoList(todoList);
   });
 };
 
 $(document).ready(function() {
+  $('.todo-add-form button').click(function(){
+    addTodo($('.todo-add-form #task').val());
+    $('.todo-add-form #task').val('');
+  });
+
   loadTodos();
 });
