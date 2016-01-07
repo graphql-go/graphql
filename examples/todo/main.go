ff3ef6984dcfc98c70a8a5fdba5749b7934766d1
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-01-07 21:19:25 +0100

    Add update todo curl example visible on app launch

--- examples/todo/main.go
+++ examples/todo/main.go
@@ -222,6 +222,7 @@ func main() {
 	fmt.Println("Now server is running on port 8080")
 	fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
 	fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
+	fmt.Println("Update todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:\"a\",done:true){id,text,done}}'")
 	fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
 	fmt.Println("Access the web app via browser at 'http://localhost:8080'")
 
